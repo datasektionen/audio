7f78502c59810ff8a29a40d37f52e959ef3a234f
Andreas Mårtensson <andreas@addem.se>
2019-04-10 21:54:49 +0200

    Possibly fix PDF rendering?

--- README.md
+++ README.md
@@ -1,19 +1,19 @@
 /dev/audio, with a PDF generator
 
 ```bash
-git clone git@github.com:addem1234/audio.git
+git clone git@github.com:datasektionen/audio.git
 cd audio
 ```
 
-Make a virtualenv here if you want
-
 Install some dependencies
+
 ```bash
-pip install -r requirements.txt
+pipenv install && pipenv shell
 npm install
 ```
 
 Start development server
+
 ```bash
 npm start && python audio.py
 ```
--- audio.py
+++ audio.py
@@ -7,9 +7,9 @@ from os import listdir
 from os.path import join, splitext
 from string import digits, ascii_letters
 
-api = responder.API(static_dir='build')
+api = responder.API(static_dir='build', static_route='/build')
 
-api.add_route("/", static=True)
+api.add_route('/', static=True)
 
 def read_song(filename):
     with open(join('songs', filename), 'r') as f: return f.read()
@@ -29,7 +29,7 @@ def pdf(req, resp, *, ext):
     cols        = whitelist(req.params.get('cols', ''), digits) or '2'
     font        = whitelist(req.params.get('font', ''), digits+ascii_letters)
     fontoptions = whitelist(req.params.get('fontoptions', ''), digits+ascii_letters)
-    songids     = req.params.get('songids').split(',')
+    songids     = req.params.get('songids', '').split(',')
 
     template = texenv.get_template('songs.tex')
     tex = template.render(
@@ -44,11 +44,12 @@ def pdf(req, resp, *, ext):
     elif ext == 'pdf':
         try:
             pdffile = build_pdf(tex)
-            resp.content_type = 'application/pdf'
+            resp.mimetype = 'application/pdf'
             resp.content = bytes(pdffile)
-        except: #LatexBuildError as e:
-            sorry = '%%Unfortunately the server failed to compile this file.\n'
-            resp.text = sorry+tex
+        except Exception as e: #LatexBuildError as e:
+            resp.mimetype = 'application/x-latex'
+            sorry = f'%%Unfortunately the server failed to compile this file: {e}\n'
+            resp.text = sorry + tex
 
 if __name__ == '__main__':
     api.run()
